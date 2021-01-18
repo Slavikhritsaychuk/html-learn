c62214f30b1ec882cb31b244ffacd18142e862a0
Slavik <slavikhritsaychuk@gmail.com>
2021-01-18 22:58:06 +0300

    lesson js

---
+++ lesson-3/script.js
@@ -0,0 +1,449 @@
+// ПЕРЕМЕННЫЕ
+
+// let c = 15;
+// let d = 2;
+// let result = c + d;
+// alert(result);
+
+// let a = 10;
+// let b = 2;
+// let c = 5;
+// alert(a + b + c);
+
+// let a = 17;
+// let b = 10;
+// let c = a - b;
+// let d = 7;
+// let result = c + d;
+// alert(result);
+
+// СТРОКИ
+
+// let str = 'привет мир'
+// alert(str);
+
+// let str1 = 'hello,';
+// let str2 = 'world!';
+// alert(str1 + str2);
+
+// let name = 'Yaroslav';
+// alert(`Hello' +[name]);
+
+// let age = 28;
+// alert("me" + [age]);
+
+// let name = prompt("Как вас зовут?", "");
+// alert('вас зовут' + [name]);
+
+// let number = prompt("введите число", "");
+// alert("квадрат етого числа" + (number * 2));
+
+// let str = 'abcde';
+// alert(str[0]);
+// alert(str[1]);
+// alert(str[2]);
+
+// let num = '12345';
+// alert(num[0] * num[1] * num[2] * num[3] * num[4]);
+
+// let hour = '18:';
+// let min = '38:';
+// let sec = '16';
+// alert("Время" + hour + min + sec);
+
+// let num = 10;
+// alert(num * 2);
+
+// let num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+
+// let arr = ['Привет,', 'мир', '!'];
+// alert(arr[0] + arr[1] + arr[2]);
+
+// let arr = ['Привет,', 'мир', '!'];
+// let text = arr[0] + arr[1];
+// alert(text);
+
+// let arr = ['Привет,', 'мир', '!'];
+// arr[0] = 'Пока';
+// alert(arr);
+
+// let obj = {
+//     'коля': '1000',
+//     'vsiya': '500',
+//     'petr': '200'
+// }
+// alert(obj['коля']);
+// alert(obj['petr']);
+
+// var arr = {
+//     'ru': ['голубой', 'красный', 'зеленый'],
+//     'en': ['blue', 'red', 'green'],
+// };
+// alert(arr['ru'][0]);
+
+// let arr = ['a', 'b', 'c'];
+// alert(arr);
+
+// let arr = ['a', 'b', 'c'];
+// alert(arr[0]);
+// alert(arr[1]);
+// alert(arr[2]);
+
+// let arr = ['a', 'b', 'c', 'd'];
+// alert(arr[0] + '+' + arr[1] + ',' + arr[2] + '+' + arr[3]);
+
+// let arr = ['2', '5', '7', '9'];
+// let result = arr[0] * arr[1] + arr[2] * arr[3];
+// alert(result);
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// };
+// alert(obj['c']);
+// alert(obj.c);
+
+// let obj = {
+//     Коля: '1000',
+//     Вася: '500',
+//     Петя: '200'
+// };
+// alert(obj.Петя);
+// alert(obj.Петя);
+
+// let week = {
+//     1: 'monday',
+//     2: 'tuesday',
+//     3: 'wednesday',
+//     4: 'thursdy',
+//     5: 'friday',
+//     6: 'saturday',
+//     7: 'sunday'
+// }
+// alert(week[5]);
+
+// let week = {
+//     1: 'monday',
+//     2: 'tuesday',
+//     3: 'wednesday',
+//     4: 'thursdy',
+//     5: 'friday',
+//     6: 'saturday',
+//     7: 'sunday'
+// }
+// let day = week[5];
+// alert(day);
+
+// let num = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+// alert(num[1][0]);
+
+// let web = {
+//     js: ['jQuery', 'Angular'],
+//     php: 'hello',
+//     css: 'world'
+// }
+// alert(web.js[0]);
+
+// let arr = {
+//     'ru': ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'субота', 'воскресение'],
+//     'en': ['Monday', 'tuesday', ' Wednesday', ' Thursday', ' Friday', ' Saturday', ' Sunday'],
+// };
+// let lang = arr['ru'];
+// let day = lang[4];
+// alert(day);
+
+// let lang = 'ru';
+// if (lang == 'ru') {
+//     let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// }
+// if (lang == 'en') {
+//     arr = ['mon', 'tue', 'wed', 'thu', 'fri', 'sut', 'sun'];
+// }
+// alert(arr);
+
+// let a = prompt('вветиде пароль', '');
+// if (a > 0) {
+//     alert('OK')
+// } else {
+//     alert('NO OK')
+// }
+// let test = prompt('введите значение', '');
+// if (test == 'true' && test == 'false') {
+//     alert('OK');
+// } else {
+//     alert('no ok');
+// }
+
+// let a = prompt('введите значение', '');
+// if (a >= 0 && a <= 5) {
+//     alert("ok");
+// } else {
+//     alert("no ok");
+// }
+
+// let a = prompt('введите значение', '');
+// if (a == 0 || a == 2) {
+//     alert(a + 7);
+// } else {
+//     alert(a / 2);
+// }
+// let a = 0;
+// let b = 5
+// if (a <= 0, b >= 3) {
+//     alert(a + b);
+// } else {
+//     alert(a - b);
+// }
+
+// let a = 3;;
+// let b = 5;
+// if (a > 2 && a < 11 || b >= 6 && b < 14) {
+//     alert('ok')
+// }
+
+// let num = 2;
+// switch (num) {
+//     case 1:
+//         let result = 'зима';
+//         break;
+//     case 2:
+//         result = 'весна';
+//         break;
+//     case 3:
+//         result = "лето";
+//         break;
+//     case 4:
+//         result = 'осень';
+//         break;
+// }
+// alert(result);
+
+// let day = 1;
+// if (day >= 0 && day <= 10) {
+//     alert('первая декада');
+// } else if (day >= 11 && day <= 20) {
+//     alert('вторая декада');
+// } else if (day >= 21 && day <= 30) {
+//     alert('третя декада');
+// } else {
+//     alert('неверно');
+// }
+
+// let month = 5;
+// if (month >= 1 && month <= 3) {
+//     alert('зима');
+// } else if (month >= 4 && month <= 6) {
+//     alert('весна');
+// } else if (month >= 7 && month <= 9) {
+//     alert('лето');
+// } else if (month >= 10 && month <= 12) {
+//     alert('осень');
+// }
+
+// let str = 'abcde';
+// if (str[0] == 'a') {
+//     alert('yes');
+// }
+
+// let str = '12345';
+// if (str[0] == '1') {
+//     alert('yes');
+// } else {
+//     alert('no');
+// }
+
+// let str = '123';
+// alert(+str[0] + +str[1] + +str[2]);
+
+// let str = '993456';
+// if (+str[0] + +str[1] + +str[2] <= +str[3] + +str[4] + +str[5]) {
+//     alert('ok');
+// } else {
+//     alert('no');
+// }
+
+// let i = 11;
+// while (i <= 33) {
+//     alert(i);
+//     i++
+// }
+
+// let result = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     result = result + i;
+// }
+// console.log(result);
+// let arr = [2, 4, 1, 5, 15, 9, 3];
+// for (let i = 0; i < 15; i++) {
+//     if (arr[i] > 3 && arr[i] < 10) {
+//         console.log(arr[i])
+//     }
+// }
+// let result = 0;
+// let arr = [2, -3, 4, -5, 5];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//         result = result + arr[i];
+//     }
+//     console.log(result);
+// }
+
+// let arr = [1, 2, 5, 9, 4, 13, 10];
+// for (let elem of arr) {
+//     if (elem == 4) {
+//         console.log('есть');
+//         break;
+//     }
+// }
+// 
+// let arr = [1, 2, 5, 9, 4, 13, 10];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == 4) {
+//         console.log('есть');
+//         break;
+//     }
+// }
+
+// let arr = [10, 20, 30, 50, 235, 3000];
+// for (let i = 0; i < arr.length; i++) {
+//     str = arr[i] + '';
+//     if (str[0] == '1' || str[0] == '2' || str[0] == '5') {
+//         console.log(str);
+//     }
+// }
+// let str = '';
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = 0; i < arr.length; i++) {
+//     str = str + '-' + arr[i];
+//     console.log(str + '-');
+// }
+// 
+
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var b = arr.length;
+// var c = ' ';
+// for (var i = 0; i < b; i++) {
+//     c = c + '-' + arr[i];
+// }
+// console.log(c + '-');
+
+// let days = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'нд'];
+// for (let i = 0; i < days.length; i++) {
+//     if (i == 5 || i == 6) {
+//         console.log(`before %c${days[i]}`, 'font-weight:bold;font-size:2rem')
+//     } else {
+//         console.log(days[i] + ' ')
+//     }
+// }
+// let day = 2;
+// let days = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'нд'];
+// for (let i = 0; i < days.length; i++) {
+//     if (i == day) {
+//         document.write("<p>Bold: " + days[i].bold() + "</p>");
+//     } else {
+//         document.write(days[i]);
+//     }
+// }
+
+// var n = 1000;
+// var num = 0;
+// while (n > 50) {
+//     num++;
+//     n = n / 2;
+// }
+// console.log(n);
+// console.log(num);
+
+// let a = 11;
+// let b = 3;
+// let result = a % b;
+// if (result != 0) {
+//     console.log('С останком' + result);
+// } else {
+//     console.log('Результата' + (a / b));
+// }
+
+
+
+// for (var num = 1, i = 0; num < 1000; num = num * 3, i++);
+// console.log(num, i);
+
+// let str = '';
+// for (let i = 0; i < 10; i++) {
+//     str = str + 'x';
+// }
+// console.log(str);
+
+// console.log(Math.pow(2, 10));// 1024
+
+// let str = Math.pow(2, 10);
+// console.log(str);
+// 
+// let result = 0;
+// let arr = [4, 2, 5, 19, 13, 0, 10];
+// for (let i = 0; i < arr.length; i++) {
+//     result = result + arr[i];
+//     console.log(Math.sqrt(result));
+// }
+
+// let str = Math.sqrt(379);
+// console.log(Math.round(str));
+// console.log(str.toFixed(2));
+// console.log(str.toFixed(3));
+
+
+
+// let num = Math.sqrt(587);
+// let obj = {};
+
+// obj.floor = Math.floor(num);
+// obj.ceil = Math.ceil(num);
+// console.log(obj);
+// console.log('hello');
+
+// console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
+// console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
+
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// console.log(getRandomInt(1, 100));
+// let arr = [{
+
+//     key1: 'BMW',
+//     key2: 'x1',
+//     key3: '2.0'
+
+// }, {
+
+//     key1: 'audi',
+//     key2: 'a5',
+//     key3: '3.0'
+
+// }];
+// console.log(arr[0].key2)
+
+// let arr = [1, 2, 3, 4, 5];
+// let result = 0;
+// for (let elem of arr) {
+//     result = result + elem;
+//     console.log(result);
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let elem of arr) {
+//     if (elem > 4) {
+//         console.log(elem);
+//     }
+// }
